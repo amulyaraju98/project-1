397686fadd1535ef76b9ccf1efb5c09e2540acad
Amulya Raju <amulyaraju461@gmail.com>
2026-05-12 17:09:53 +0000

    created macros

---
+++ models/example/Staging/stg_shipping.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized='table',
+    schema='staging'
+) }}
+SELECT
+    PRODUCT,
+    TO_DATE(SHIP_DATE, 'DD/MM/YY')         AS SHIP_DATE,
+    TO_DATE(DELIVERED_DATE, 'DD/MM/YY')    AS DELIVERED_DATE
+FROM {{ source('Amulya_sources', 'SHIPPING') }}
