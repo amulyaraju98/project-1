6825ab0bf97090cb0b33d50cbb9df91d66adc732
Amulya R <amulyaraju461@gmail.com>
2026-06-02 14:15:35 +0000

    changes

--- models/lm2.sql
+++ models/lm2.sql
@@ -1,11 +1,11 @@
-{% set list_of_countries=dbt_utils.get_column_values(source('Amulya_sources', 'Raw_users'), 'country')%}
+-- {% set list_of_countries=dbt_utils.get_column_values(source('Amulya_sources', 'Raw_users'), 'country')%}
 
-{% for i in list_of_countries %}
-{{i}}
-{% endfor %}
-{{list_of_countries}}
+-- {% for i in list_of_countries %}
+-- {{i}}
+-- {% endfor %}
+-- {{list_of_countries}}
 
-{% set list_of_columns=adapter.get_columns_in_relation(source('Amulya_sources', 'Raw_users')) %}
-{% for i in list_of_columns %}
-{{i.column}}
-{% endfor %}
+-- {% set list_of_columns=adapter.get_columns_in_relation(source('Amulya_sources', 'Raw_users')) %}
+-- {% for i in list_of_columns %}
+-- {{i.column}}
+-- {% endfor %}
